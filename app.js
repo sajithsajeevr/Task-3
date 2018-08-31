d2c88ea043c38491d2718425383f54ab4083aa9a
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-31 14:57:47 +0530

    added paragraph to display given array

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 const uList=document.querySelector('#uList');
 //given array
 const array=['Apple','Grape','Orange'];
-
+document.getElementById('givenArray').innerHTML=`<em>Given Array:[${array[0]},${array[1]},${array[2]}]<em>`;
 array.forEach(fruit=>uList.innerHTML+=`<li>${fruit}</li>`);
